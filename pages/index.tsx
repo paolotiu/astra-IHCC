0e812049240e7ec55601fded3f0dbcc1b7ee1191
Paolo Tiu <paolotiu17@gmail.com>
2021-07-16 19:16:58 +0800

    added customer selection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,43 @@
 import Layout from '@components/Layout';
-import { Button, makeStyles } from '@material-ui/core';
+import { Button, makeStyles, Typography, Box } from '@material-ui/core';
 import Link from 'next/link';
+import React from 'react';
 
 const useStyles = makeStyles({
-  grid: {
+  flex: {
     display: 'flex',
     height: '100vh',
     flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    display: 'grid',
+    gap: '1.8rem',
+    '& .MuiButton-containedPrimary': {
+      color: 'white',
+    },
   },
 });
 const IndexPage = () => {
   const classes = useStyles();
   return (
     <Layout title="Astra">
-      <div className={classes.grid}>
-        <Link href="/book">
-        <Button>Login as Customer</Button>
-</Link>
+      <div className={classes.flex}>
+        <Box paddingBottom={4}>
+          <Typography variant="h6">Are you a buyer or a seller?</Typography>
+        </Box>
+        <div className={classes.buttonContainer}>
+          <Button variant="contained" color="primary">
+            I am a Buyer
+          </Button>
 
-        <Button>Login as Business</Button>
+          <Link href="/book">
+            <Button variant="contained" color="primary">
+              I am a Seller
+            </Button>
+          </Link>
+        </div>
       </div>
     </Layout>
   );
